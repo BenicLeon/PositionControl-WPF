959646d7369f6a69c5bc2e6e561868eb99f931b4
Leon Benić <64423174+BenicLeon@users.noreply.github.com>
2024-10-02 19:31:23 +0200

    Aded Login, User logic and admin logic

--- Orqa/PositionControl/Views/UserView.xaml.cs
+++ Orqa/PositionControl/Views/UserView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using PositionControl.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,21 +13,17 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PositionControl
+namespace PositionControl.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UserView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserView : UserControl
     {
-        public MainWindow()
+        public UserView()
         {
             InitializeComponent();
-
-            DataContext = new ViewModels.LoginViewModel();
-
-
+            DataContext = new ViewModels.UserViewModel();
         }
-       
     }
 }
