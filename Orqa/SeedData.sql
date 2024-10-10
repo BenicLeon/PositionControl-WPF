718b3bba203260cf33a8c48055ef408f73f396e1
Leon Benić <64423174+BenicLeon@users.noreply.github.com>
2024-10-10 18:03:30 +0200

    Fixed hash password login

--- Orqa/SeedData.sql
+++ Orqa/SeedData.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO roles (Id,RoleName,RoleDescription) VALUES (11,'Admin','Adminova rola'),(12,'User','Userova Rola');
 
-INSERT INTO users (Id,FirstName,LastName,Username,PaswordHash,RoleId) VALUES (7,'Leon','Benić','Lbenic','lozinka',11),(8,'Marko','Marić','Mmaric','lozinka',12);
+INSERT INTO users (Id,FirstName,LastName,Username,PaswordHash,RoleId) VALUES (7,'Leon','Benić','Lbenic','$2y$10$dz6s5bDX/htE68UUhPBC5.6/G0lHpReWtQfx/B0ZnmwJCKfF1PRAC',11),(8,'Marko','Marić','Mmaric','$2y$10$dz6s5bDX/htE68UUhPBC5.6/G0lHpReWtQfx/B0ZnmwJCKfF1PRAC',12);
 
 INSERT INTO workpositions (Id,PositionName,PositionDescription) VALUES (8,'Glavni radnik','Upravljanje korisnicima'),(9,'Obićni radnik','Strojevi');
 
